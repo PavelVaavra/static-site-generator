efdc0a6f1343a11716a98fba504b3e4b73f3890e
PavelVaavra <pavelvavra.vm@gmail.com>
2025-06-05 22:15:00 +0200

    block_to_block_type function - heading and code types

--- src/blocks_functions.py
+++ src/blocks_functions.py
@@ -1,3 +1,28 @@
+import re
+
+from enum import Enum
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return [block.strip() for block in blocks if len(block) != 0]
+
+class BlockType(Enum):
+    PARAGRAPH_BLOCK_TYPE = "paragraph"
+    HEADING_BLOCK_TYPE = "heading"
+    CODE_BLOCK_TYPE = "code"
+    QUOTE_BLOCK_TYPE = "quote"
+    UNORDERED_LIST_BLOCK_TYPE = "unordered_list"
+    ORDERED_LIST_BLOCK_TYPE = "ordered_list"
+
+def block_to_block_type(block):
+    if re.search(r"^#{1,6} .+", block):
+        return BlockType.HEADING_BLOCK_TYPE
+    if block[:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE_BLOCK_TYPE
+    quote, unordered = True, True
+    for line in block.split("\n"):
+        print(line[0])
+        if line[0] != ">":
+            quote = False
+    if quote:
+        return BlockType.QUOTE_BLOCK_TYPE
--- src/test_blocks_functions.py
+++ src/test_blocks_functions.py
@@ -1,6 +1,10 @@
 import unittest
 
-from blocks_functions import markdown_to_blocks
+from blocks_functions import (
+    markdown_to_blocks,
+    BlockType,
+    block_to_block_type
+)
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -24,6 +28,65 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    # def test_paragraph(self):
+    #     block = "Here's the deal, **I like Tolkien**."
+    #     self.assertEqual(
+    #         block_to_block_type(block),
+    #         BlockType.PARAGRAPH_BLOCK_TYPE
+    #     )
+    
+    def test_heading(self):
+        block = "## Reasons I like Tolkien"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING_BLOCK_TYPE
+        )
+
+    def test_code(self):
+        block = """```
+func main(){
+    fmt.Println("Aiya, Ambar!")
+}
+```"""
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.CODE_BLOCK_TYPE
+        )
+
+#     def test_quote(self):
+#         block = """> "I am in fact a Hobbit in all but size."
+# >
+# > -- J.R.R. Tolkien
+# """
+#         self.assertEqual(
+#             block_to_block_type(block),
+#             BlockType.QUOTE_BLOCK_TYPE
+#         )
+# 
+#     def test_unordered_list(self):
+#         block = """- You can spend years studying the legendarium and still not understand its depths
+# - It can be enjoyed by children and adults alike
+# - Disney _didn't ruin it_ (okay, but Amazon might have)
+# - It created an entirely new genre of fantasy
+# """
+#         self.assertEqual(
+#             block_to_block_type(block),
+#             BlockType.UNORDERED_LIST_BLOCK_TYPE
+#         )
+
+#     def test_ordered_list(self):
+#         block = """1. Gandalf
+# 2. Bilbo
+# 3. Sam
+# 4. Glorfindel
+# 5. Galadriel
+# """
+#         self.assertEqual(
+#             block_to_block_type(block),
+#             BlockType.ORDERED_LIST_BLOCK_TYPE
+#         ) 
 
 if __name__ == "__main__":
     unittest.main()
