df7459bb71dc43c3c88f7c12e9347910e3bd2351
PavelVaavra <pavelvavra.vm@gmail.com>
2025-06-05 21:07:23 +0200

    files renaming

--- src/test_inline_functions.py
+++ src/test_inline_functions.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from functions import (
+from inline_functions import (
     text_node_to_html_node, 
     split_nodes_delimiter, 
     extract_markdown_images,
