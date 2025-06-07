84888170c3b1b8e68192db9e231b484614748d6c
PavelVaavra <pavelvavra.vm@gmail.com>
2025-06-07 23:47:35 +0200

    markdown_to_html_node paragraph, code, (un)ordered list

--- src/blocks_functions.py
+++ src/blocks_functions.py
@@ -1,6 +1,8 @@
 import re
-
 from enum import Enum
+
+from inline_functions import text_to_textnodes, text_node_to_html_node
+from htmlnode import ParentNode, LeafNode
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -40,3 +42,65 @@ def block_to_block_type(block):
             if is_ordered_list:
                 return BlockType.ORDERED_LIST_BLOCK_TYPE
     return BlockType.PARAGRAPH_BLOCK_TYPE
+
+def markdown_to_html_node(markdown):
+    """
+    blocks:
+        - paragraph
+        - heading
+        - code
+        - quote
+        - unordered_list
+        - ordered_list
+    inline:
+        - normal text
+        - bold text
+        - italic text
+        - code text
+        - link
+        - image
+    """
+    blocks = markdown_to_blocks(markdown)
+    blocks_html = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.PARAGRAPH_BLOCK_TYPE:
+                block = block.replace("\n", " ")
+                text_nodes = text_to_textnodes(block)
+                html_nodes = []
+                for text_node in text_nodes:
+                    html_nodes.append(text_node_to_html_node(text_node))
+                blocks_html.append(ParentNode("p", html_nodes))
+            case BlockType.HEADING_BLOCK_TYPE:
+                pass
+            case BlockType.CODE_BLOCK_TYPE:
+                blocks_html.append(ParentNode("pre", [LeafNode("code", block[3:-3].lstrip())]))
+            case BlockType.QUOTE_BLOCK_TYPE:
+                pass
+            case BlockType.UNORDERED_LIST_BLOCK_TYPE:
+                lines = block.split("\n")
+                lines = [line.split(" ", 1)[-1] for line in lines]
+                html_nodes = []
+                for line in lines:
+                    text_nodes = text_to_textnodes(line)
+                    line_html_nodes = []
+                    for text_node in text_nodes:
+                        line_html_nodes.append(text_node_to_html_node(text_node))
+                    html_nodes.append(ParentNode("li", line_html_nodes))
+                blocks_html.append(ParentNode("ul", html_nodes))
+            case BlockType.ORDERED_LIST_BLOCK_TYPE:
+                lines = block.split("\n")
+                lines = [line.split(" ", 1)[-1] for line in lines]
+                html_nodes = []
+                for line in lines:
+                    text_nodes = text_to_textnodes(line)
+                    line_html_nodes = []
+                    for text_node in text_nodes:
+                        line_html_nodes.append(text_node_to_html_node(text_node))
+                    html_nodes.append(ParentNode("li", line_html_nodes))
+                blocks_html.append(ParentNode("ol", html_nodes))
+            case _:
+                raise ValueError
+    return ParentNode("div", blocks_html)
+    
--- src/test_blocks_functions.py
+++ src/test_blocks_functions.py
@@ -3,7 +3,8 @@ import unittest
 from blocks_functions import (
     markdown_to_blocks,
     BlockType,
-    block_to_block_type
+    block_to_block_type,
+    markdown_to_html_node
 )
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -91,7 +92,75 @@ func main(){
         self.assertEqual(
             block_to_block_type(block),
             BlockType.ORDERED_LIST_BLOCK_TYPE
-        ) 
+        )
+
+class TestMarkdownToHtmlNode(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_ordered_list(self):
+        md = """
+1. Gandalf [boot.dev](https://boot.dev)
+2. Bilbo **bold**
+3. Sam
+4. Glorfindel
+5. Galadriel
+6. Elrond
+7. Thorin _italic_ ahoj
+8. Sauron
+9. ![Aragorn](src/images/img.png)
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            '<div><ol><li>Gandalf <a href="https://boot.dev">boot.dev</a></li><li>Bilbo <b>bold</b></li><li>Sam</li><li>Glorfindel</li><li>Galadriel</li>'
+            '<li>Elrond</li><li>Thorin <i>italic</i> ahoj</li><li>Sauron</li><li><img src="src/images/img.png" alt="Aragorn"></li></ol></div>',
+        )
+
+    def test_unordered_list(self):
+        md = """
+- You can spend years studying the legendarium and still not understand its depths
+- It can be enjoyed by children and adults alike
+- Disney _didn't ruin it_ (okay, but Amazon might have)
+- It created an entirely new genre of fantasy
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            '<div><ul><li>You can spend years studying the legendarium and still not understand its depths</li>'
+            '<li>It can be enjoyed by children and adults alike</li>'
+            '<li>Disney <i>didn\'t ruin it</i> (okay, but Amazon might have)</li>'
+            '<li>It created an entirely new genre of fantasy</li></ul></div>',
+        )
 
 if __name__ == "__main__":
     unittest.main()
